487bcd8aa200c4337425bbf2b954e97a4c39b48a
Martin Herbert <martinherbert93@gmail.com>
2020-05-29 15:47:39 -0400

    Restore pipfile.lock and revert flask-cors

--- flask_api/__init__.py
+++ flask_api/__init__.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 import psycopg2
-from flask_cors import CORS
 
 from flask_api.routes.spotify_routes import spotify_routes
 
@@ -10,7 +9,6 @@ from flask_api.routes.spotify_routes import spotify_routes
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
 
     app.register_blueprint(spotify_routes)
 
