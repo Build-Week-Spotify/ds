2c57ef6d5e40b76db4c2575c16c5df12b1bcae63
Martin Herbert <martinherbert93@gmail.com>
2020-05-29 13:42:48 -0400

    Install flask-cors.

--- flask_api/__init__.py
+++ flask_api/__init__.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 import psycopg2
+from flask_cors import CORS
 
 from flask_api.routes.spotify_routes import spotify_routes
 
@@ -9,6 +10,7 @@ from flask_api.routes.spotify_routes import spotify_routes
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     app.register_blueprint(spotify_routes)
 
